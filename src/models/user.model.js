3fa1c09ffe309bf9630ff0dc273e871846ab1720
uday54565 <rajakudaykumar27@gmail.com>
2026-07-16 10:36:34 +0530

    user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
-// import { Schema } from "mongoose";
 import mongoose , {Schema} from "mongoose";
+import bcrypt from "bcrypt";
+import { JsonWebTokenError } from "jsonwebtoken";
 
 const userSchema = new Schema (
 
@@ -51,7 +52,44 @@ const userSchema = new Schema (
             type : String
         }
 
-}, {timestamps : true}
+}, {timestamps : true},
+
+userSchema.pre( "save" , async function (next) {
+    if(!this.isModifyed("password")) return next();
+
+    this.password = bcrypt.hash(this.password , 10);
+    next();
+},
+userSchema.methods.ispasswordcorrect  = async function (password) {
+
+ return    await   bcrypt.compare(password,this.password);
+    
+},
+
+userSchema.methods.genrateAccessTokan = function (){
+    jwt.sign({
+
+        _id : this._id,
+        email : this.email,
+        fullname : this.fullname,
+        userName : this.fullname
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIN : process.env.ACCESS_TOKEN_EXPIRY
+    }
+
 )
+
+},
+
+userSchema.methods.genrateRefrenceToken = function () {
+     {
+        expiresIN : process.env.ACCESS_TOKEN_EXPIRY
+        
+    }
+}
+
+))
 
 export const User = mongoose.model("User" , userSchema);
